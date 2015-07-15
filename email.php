521856ebaef6a21d3c00068eeb5264328c0cbf81
manaiajunior <manaiajr.ifrn@gmail.com>
2015-07-14 22:39:23 -0300

    Correção dos dados de acesso ao email.

--- email.php
+++ email.php
@@ -1,13 +1,13 @@
 <?php
-/* Inclui a classe do phpmailer */				
+/* Inclui a classe do phpmailer */
 require("phpmailer/class.phpmailer.php");
- 
+
 /* Cria uma Instância da classe */
 $mail = new PHPMailer();
 
 
 /* #########################
- * # CONFIGURAÇÕES BÁSICAS # 
+ * # CONFIGURAÇÕES BÁSICAS #
  * #########################
  */
 
@@ -19,36 +19,36 @@ $matricula = $_POST["nMatricula"];
 $senha = $_POST["nSenha"];
 
 $mensagem = "<h3>DADOS CADASTRADOS</h3><br/>
-			 <p><strong>Nome Completo: </strong>".$nome."</p><br/> 
-			 <p><strong>Email: </strong>".$email."</p><br/> 
-			 <p><strong>Telefone: </strong>".$telefone."</p><br/> 
-			 <p><strong>Secretaria: </strong>".$secretaria."</p><br/> 
-			 <p><strong>Matricula: </strong>".$matricula."</p><br/> 
+			 <p><strong>Nome Completo: </strong>".$nome."</p><br/>
+			 <p><strong>Email: </strong>".$email."</p><br/>
+			 <p><strong>Telefone: </strong>".$telefone."</p><br/>
+			 <p><strong>Secretaria: </strong>".$secretaria."</p><br/>
+			 <p><strong>Matricula: </strong>".$matricula."</p><br/>
 			 <p><strong>Senha: </strong>".$senha."</p>";
 
-$seu_email = 'manaiajr.ifrn@gmail.com';
-$seu_nome = 'Manaia Junior';
-$sua_senha = 'Leal@230297';
- 
+$seu_email = 'email@gmail.com';
+$seu_nome = 'Nome Test';
+$sua_senha = '12345';
+
 /* Se for do Gmail o servidor é: smtp.gmail.com */
 $host_do_email = 'smtp.gmail.com';
- 
+
 /* Configura os destinatários (pra quem vai o email) */
 $mail->AddAddress($email, $nome);
 // $mail->AddAddress('email@email.com');
 // $mail->AddCC('email@email.com', 'Nome da pessoa'); // Copia
 // $mail->AddBCC('email@email.com', 'Nome da pessoa'); // Cópia Oculta
- 
+
 /* ###########################
- * # CONFIGURAÇÕES AVANÇADAS # 
+ * # CONFIGURAÇÕES AVANÇADAS #
  * ###########################
  */
-				
+
 /* Define que é uma conexão SMTP */
 $mail->IsSMTP();
 /* Define o endereço do servidor de envio */
 $mail->Host = $host_do_email;
-/* Utilizar autenticação SMTP */ 
+/* Utilizar autenticação SMTP */
 $mail->SMTPAuth = true;
 /* Protocolo da conexão */
 $mail->SMTPSecure = "ssl";
@@ -58,35 +58,35 @@ $mail->Port = "465";
 $mail->Username = $seu_email;
 /* Senha do usuário */
 $mail->Password = $sua_senha;
- 
+
 /* Configura os dados do remetente do email */
 $mail->From = $seu_email; // Seu e-mail
 $mail->FromName = $seu_nome; // Seu nome
- 
+
 /* Configura a mensagem */
 $mail->IsHTML(true); // Configura um e-mail em HTML
- 
-/*   
+
+/*
  * Se tiver problemas com acentos, modifique o charset
- * para ISO-8859-1  
+ * para ISO-8859-1
  */
 $mail->CharSet = 'UTF-8'; // Charset da mensagem (opcional)
- 
+
 /* Configura o texto e assunto */
 $mail->Subject  = "CADASTRO DE USUÁRIO - ".$nome; // Assunto da mensagem
 $mail->Body = $mensagem; // A mensagem em HTML
 $mail->AltBody = trim(strip_tags($mensagem)); // A mesma mensagem em texto puro
- 
+
 /* Configura o anexo a ser enviado (se tiver um) */
 //$mail->AddAttachment("foto.jpg", "foto.jpg");  // Insere um anexo
- 
+
 /* Envia o email */
 $email_enviado = $mail->Send();
- 
+
 /* Limpa tudo */
 $mail->ClearAllRecipients();
 $mail->ClearAttachments();
- 
+
 /* Mostra se o email foi enviado ou não */
 if ($email_enviado) {
 	echo "Email enviado!";
